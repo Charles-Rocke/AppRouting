e23250887a00cbfd0cc9258a453e50fa1898203c
Spook <spook.devv@gmail.com>
2024-03-20 14:47:30 -0400

    Configuring dynamic routes and useing route parameters

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,11 @@
+function BlogPostPage({ params }) {
+  console.log(params);
+  return (
+    <main>
+      <h1>Blog Post</h1>
+      <p>{params.slug}</p>
+    </main>
+  );
+}
+
+export default BlogPostPage;
